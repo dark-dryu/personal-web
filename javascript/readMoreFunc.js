59d1a0ca2bda84637f5813a29c597d630eaae181
Andreu Anglés-Castillo <andreu@portatil.localdomain>
2021-03-17 17:31:51 +0100

    Estil consolidad i adaptat a movils

---
+++ javascript/readMoreFunc.js
@@ -0,0 +1,47 @@
+function readMoreFunc1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("BtnReadMore1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "···"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "hide"; 
+      moreText.style.display = "inline";
+    }
+}
+
+function readMoreFunc2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("BtnReadMore2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "···"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "hide"; 
+      moreText.style.display = "inline";
+    }
+}
+
+function readMoreFunc3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("BtnReadMore3");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "···"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "hide"; 
+      moreText.style.display = "inline";
+    }
+}
