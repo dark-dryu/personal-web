04c4b81c7cc8a070e76f48b89801684c929aa04f
dark-dryu <andreu.cs.95@gmail.com>
2021-09-07 18:47:35 +0200

    Homepage for firefox

---
+++ config.js
@@ -0,0 +1,123 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+  // ├┴┐├─┤└─┐││  └─┐
+  // └─┘┴ ┴└─┘┴└─┘└─┘
+
+  // General
+  name: 'Andreu',
+  imageBackground: false,
+  openInNewTab: false,
+
+  // Greetings
+  greetingMorning: 'Bon diaaa!!',
+  greetingAfternoon: 'Recordat de verenar',
+  greetingEvening: 'Naniiit',
+  greetingNight: 'Ale xato, vesten a dormir!',
+
+  // Weather
+  weatherKey: '6ca382144414eeeca845f7964272cdeb',
+  weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
+  weatherUnit: 'C',
+  weatherLatitude: '40',
+  weatherLongitude: '0',
+  
+
+  // ┌─┐┌─┐┬─┐┌┬┐┌─┐
+  // │  ├─┤├┬┘ ││└─┐
+  // └─┘┴ ┴┴└──┴┘└─┘
+
+  // Links
+  cards: [
+    {
+      id: '1',
+      name: 'WhatsApp',
+      icon: 'message-circle',
+      link: 'https://web.whatsapp.com/',
+    },
+    {
+      id: '2',
+      name: 'Youtube',
+      icon: 'youtube',
+      link: 'https://www.youtube.com',
+    },
+    {
+      id: '3',
+      name: 'Reddit',
+      icon: 'bookmark',
+      link: 'https://reddit.com',
+    },
+    {
+      id: '4',
+      name: 'Snahp.it',
+      icon: 'film',
+      link: 'https://forum.snahp.it/',
+    },
+    {
+      id: '5',
+      name: 'Gmail',
+      icon: 'mail',
+      link: 'https://gmail.com',
+    },
+    {
+      id: '6',
+      name: 'Mural Andreu',
+      icon: 'user',
+      link: 'https://mural.uv.es/anancas2',
+    },
+  ],
+
+  // ┬  ┬┌─┐┌┬┐┌─┐
+  // │  │└─┐ │ └─┐
+  // ┴─┘┴└─┘ ┴ └─┘
+
+  //Icons
+  firstListIcon: 'crosshair',
+  secondListIcon: 'music',
+
+  // Links
+  lists: {
+    firstList: [
+      {
+        name: 'Pokemon',
+        link: 'https://play.pokemonshowdown.com',
+      },
+      {
+        name: 'Chess',
+        link: 'https://www.chess.com',
+      },
+      {
+        name: 'Letterboxd',
+        link: 'https://letterboxd.com/',
+      },
+      {
+        name: 'Rock',
+        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      },
+    ],
+    secondList: [
+      {
+        name: 'Lo-Fi',
+        link: 'https://www.youtube.com/watch?v=5qap5aO4i9A',
+      },
+      {
+        name: 'Classica',
+        link: 'https://www.youtube.com/watch?v=_3IphE64yRA',
+      },
+      {
+        name: 'Hand curated',
+        link: 'https://open.spotify.com/playlist/6CEKMRtHQtiEDd0tG9ndCu?si=470f407308c44f66',
+      },
+      {
+        name: 'Familia',
+        link: 'https://open.spotify.com/playlist/6eDGBHrMJ8Ir2C7AYbuydc?si=aab294bf1eb742ed',
+      },
+    ],
+  },
+};
